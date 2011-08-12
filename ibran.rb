98cd62091682700d90bed80b464fa9a746973964
Muke Tever <muke@frath.net>
2011-08-11 18:51:12 -0600

    Rule #1 - drop final m

--- ibran.rb
+++ ibran.rb
@@ -2,11 +2,14 @@
 
 def ibranify latin
 	# Derive orthography from input
+	pron = latin.dup
 	orth = latin.delete ":"
-	puts " 0: = " + latin.ljust(20) + orth
+	puts " 0: = " + pron.ljust(20) + orth
 
-	# Word-final /m/ -> 0
-	
+	# 1: Word-final /m/ -> 0
+	hit_rule =	pron.gsub! /m$/, ''
+							orth.gsub! /m$/, ''
+	puts " 1: = " + pron.ljust(20) + orth if hit_rule
 end
 
 ibranify ARGV[0]
