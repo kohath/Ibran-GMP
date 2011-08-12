fe9a425bc5bed966940d06577387cd2ed2275af5
Muke Tever <muke@frath.net>
2011-08-11 18:37:24 -0600

    Initial commit

---
+++ ibran.rb
@@ -0,0 +1,12 @@
+﻿#!/usr/bin/env ruby
+
+ibranify ARGV[0]
+
+def ibranify latin
+	# Derive orthography from input
+	orth = latin.delete ":"
+	puts " 0: = " + latin.ljust(20) + orth
+
+	# Word-final /m/ -> 0
+	
+end
