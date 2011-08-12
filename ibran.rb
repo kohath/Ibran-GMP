55497a79acf14f14ebe2214a1c1e90ce52bdb8e5
Muke Tever <muke@frath.net>
2011-08-11 19:37:18 -0600

    rules 3 & 4; also, fix #change to properly handle multiple changes

--- ibran.rb
+++ ibran.rb
@@ -11,15 +11,23 @@ def ibranify latin
 	
 	# 2: /mn/ -> /nn/
 	change 2, {/mn/ => 'nn'}
+	
+	# 3: Word-final /t/ -> 0
+	change 3, {/t$/ => ''}
+	
+	# 4: /h/ to 0 (and unwritten /ph/ -> f)
+	change 4, {/ph/ => 'f', /h/ => ''}
 end
 
 def change rule, pron_changes, orth_changes = pron_changes
 	hit_phon = pron_changes.keys.inject(nil) do |hit, key|
-		hit ||= @pron.gsub! key, pron_changes[key]
+		hit_key = @pron.gsub! key, pron_changes[key]
+		hit ||= hit_key
 	end
 
 	hit_orth = orth_changes.keys.inject(nil) do |hit, key|
-		hit ||= @orth.gsub! key, orth_changes[key]
+		hit_key = @orth.gsub! key, orth_changes[key]
+		hit ||= hit_key
 	end
 
 	print_change rule, @pron, @orth if hit_phon || hit_orth
