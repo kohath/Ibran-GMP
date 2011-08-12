f9b76f810ac7bca99a9894b038ba71f163da6670
Muke Tever <muke@frath.net>
2011-08-11 18:58:43 -0600

    Rule 2: mn -> nn

--- ibran.rb
+++ ibran.rb
@@ -10,6 +10,11 @@ def ibranify latin
 	hit_rule =	pron.gsub! /m$/, ''
 							orth.gsub! /m$/, ''
 	print_change 1, pron, orth if hit_rule
+	
+	# 2: /mn/ -> /nn/
+	hit_rule = pron.gsub! /mn/, 'nn'
+						 orth.gsub! /mn/, 'nn'
+	print_change 2, pron, orth if hit_rule
 end
 
 def print_change rule, pron, orth
